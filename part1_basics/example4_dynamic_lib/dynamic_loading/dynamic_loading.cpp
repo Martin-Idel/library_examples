b799e49c00c87cb4d6426446fbf8961d8e1479eb
Martin Idel <martin.idel@googlemail.com>
2018-08-11 20:52:24 +0200

    Change to `extern "C"` usage

--- part1_basics/example4_dynamic_lib/dynamic_loading/dynamic_loading.cpp
+++ part1_basics/example4_dynamic_lib/dynamic_loading/dynamic_loading.cpp
@@ -8,18 +8,22 @@
 int main()
 {
 	void * my_lib_handle = dlopen("libfirst_shared.so",RTLD_NOW);
+
 	if(my_lib_handle==NULL) {
 		std::cout << "Library could not be opened" << std::endl;
         return 0;
 	}
-    // Don't do this in real life, use `extern "C"` in the library to use the real name here
-    uint64_t (*addingTwoNumbers)(uint64_t, uint64_t) = reinterpret_cast<uint64_t (*)(uint64_t, uint64_t)>(dlsym(my_lib_handle,"_ZN6my_lib16addingTwoNumbersEmm"));
+
+    uint64_t (*addingTwoNumbers)(uint64_t, uint64_t) = reinterpret_cast<uint64_t (*)(uint64_t, uint64_t)>(dlsym(my_lib_handle,"addingTwoNumbers"));
+
 	if(addingTwoNumbers==NULL) {
 		std::cout << "Function could not be loaded" << std::endl;
         dlclose(my_lib_handle);
         return 0;
 	}
+
     std::cout << (*addingTwoNumbers)(10, 20) << std::endl;
+
     dlclose(my_lib_handle);
 	return 0;
 }
--- part1_basics/example4_dynamic_lib/first_library/first_library.hpp
+++ part1_basics/example4_dynamic_lib/first_library/first_library.hpp
@@ -2,5 +2,5 @@
 
 namespace my_lib 
 {
-    uint64_t addingTwoNumbers(uint64_t a, uint64_t b);
+    extern "C" uint64_t addingTwoNumbers(uint64_t a, uint64_t b);
 }
